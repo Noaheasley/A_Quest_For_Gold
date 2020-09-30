6f8cee17f6d1725fe23c35b642cf65652869cf68
Noah <noaheasley408@gmail.com>
2020-09-29 19:54:13 -0500

    set up player inventory and initialized items for the player

--- Item.cs
+++ Item.cs
@@ -1,11 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 
 namespace HelloWorld
 {
     class Item
     {
+        public string _name;
+        public int _statBoost;
+        private Item _longSword;
+        private Item _claymore;
+        private Item _bow;
+        private Item _crossBow;
+        private Item _enchantedSword;
+        private Item _zweihandler;
 
+        public Item()
+        {
+            _statBoost = 25;
+        }
+
+        public Item(string nameVal, int statBoost)
+        {
+            _name = nameVal;
+        }
+
+        //intializes all weapons that the player can buy and use
+        public void IntialItems()
+        {
+            _longSword._name = "Longsword";
+            _longSword._statBoost = 15;
+            _claymore._name = "Claymore";
+            _claymore._statBoost = 25;
+            _bow._name = "Bow";
+            _bow._statBoost = 20;
+            _crossBow._name = "Crossbow";
+            _crossBow._statBoost = 30;
+            _enchantedSword._name = "Enchanted Sword";
+            _enchantedSword._statBoost = 25;
+            _zweihandler._name = "Zweihandler";
+            _zweihandler._statBoost = 40;
+        }
+
+        public void GetInput(out char input, string option1, string option2, string query)
+        {
+            input = ' ';
+
+            Console.WriteLine(query);
+            Console.WriteLine("1. " + option1);
+            Console.WriteLine("2. " + option2);
+            while (input != '1' && input != '2')
+            {
+                input = Console.ReadKey().KeyChar;
+                if (input != '1' && input != '2')
+                {
+                    Console.WriteLine("invalid input");
+                }
+            }
+        }
+
+        public int GetStatBoost()
+        {
+            return _statBoost;
+        }
     }
 }
--- Player.cs
+++ Player.cs
@@ -4,9 +4,53 @@ using System.Text;
 
 namespace HelloWorld
 {
-    class Player
+    class Player : Character
     {
         private int _gold;
         private Item[] _inventory;
+        private Item _currentWeapon;
+        private Item _hands;
+
+        public Player() : base()
+        {
+            _inventory = new Item[3];
+
+            _hands._name = "fists";
+            _hands._statBoost = 0;
+        }
+
+        public Player(string nameVal, float healthVal, float damageVal, int inventorySize)
+            : base(healthVal, nameVal, damageVal)
+        {
+            _inventory = new Item[inventorySize];
+            _hands._name = "fists";
+            _hands._statBoost = 0;
+        }
+
+        public void UnequipItem()
+        {
+            _currentWeapon = _hands;
+        }
+        public bool Contains(int itemIndex)
+        {
+            if(itemIndex > 0 && itemIndex < _inventory.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+        //adds an item to the inventory index
+        public void AddItemToInv(Item item, int index)
+        {
+            _inventory[index] = item;
+        }
+        //allows the player to equip an item from their inventory
+        public void EquipItem(int itemIndex)
+        {
+            if (Contains(itemIndex))
+            {
+                _currentWeapon = _inventory[itemIndex];
+            }
+        }
     }
 }
